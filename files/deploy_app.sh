726c9abddb94f1eefd29491cada83afb54062d85
malekovic <l.malekovic@gmail.com>
2021-02-10 14:08:06 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,8 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  <h1>ACME</h1>
+  <p>Supplying anti-roadrunner ordnance for over a century.</p>
   <!-- END -->
 
   </div>
